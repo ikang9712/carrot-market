65c1f05ccb113436749af75db389b8fe7493aa3e
Inho Kang <ikang9712@gmail.com>
2023-04-13 18:29:19 -0400

    feature/Sending SMS using Twilio

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,13 +1,45 @@
 
+import client from "@/libs/server/client";
 import { NextApiRequest, NextApiResponse } from "next";
 import withHandler from '../../../libs/server/withHandler';
+import { ResponseType } from '../../../libs/server/withHandler';
+import twilio from "twilio";
 
+const twilioClient = twilio(process.env.TWILIO_SID,process.env.TWILIO_TOKEN)
 
-
-
-async function handler(req: NextApiRequest, res: NextApiResponse) {
-  console.log(req.body);
-  return res.status(200).end();
+async function handler(req: NextApiRequest, res: NextApiResponse<ResponseType>) {
+  const { phone, email } = req.body;
+  const user = phone ? { phone: +phone } : email? { email } : null;
+  if (!user) return res.status(400).json({ok:false});
+  const payload = Math.floor(100000 + Math.random() * 900000) + "";
+  const token = await client.token.create({
+    data: {
+      payload,
+      user: {
+        connectOrCreate: {
+          where: {
+      ...user,
+    },
+    create: {
+      name: "Anonymous",
+      ...user,
+    },
+        }
+      }
+    }
+  });
+  if (phone){
+    const message = await twilioClient.messages.create({
+      messagingServiceSid: process.env.TWILIO_MSID,
+      to: process.env.MY_PHONE!,
+      body: `Your login token is ${payload}`
+    })
+    console.log(message)
+  }
+  
+  return res.json({
+    ok: true
+  })
 }
 
 export default withHandler("POST", handler);
