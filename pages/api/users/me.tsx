16af568a3f4306e7704ee286ef924774d391e724
Inho Kang <ikang9712@gmail.com>
2023-04-13 21:11:41 -0400

    Encrypted session using IronSession for Auth Token

--- pages/api/users/me.tsx
+++ pages/api/users/me.tsx
@@ -3,33 +3,24 @@ import { NextApiRequest, NextApiResponse } from "next";
 import withHandler, { ResponseType } from "@/libs/server/withHandler";
 import client from "@/libs/server/client";
 
-declare module "iron-session" {
-  interface IronSessionData {
-    user?: {
-      id: number;
-    };
-  }
-}
 
 async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseType>
 ) {
-  const { token } = req.body;
-  const exists = await client.token.findUnique({
-    where: {
-      payload: token,
-    },
-  });
-  if (!exists) return res.status(404).end();
-  req.session.user = {
-    id: exists.userId,
-  };
-  await req.session.save();
-  res.status(200).end();
+  console.log(req.session.user);
+  const profile = await client.user.findUnique({
+      where: {
+          id: req.session.user?.id
+      }
+  })
+  res.json({
+      ok:true,
+      profile
+  })
 }
 
-export default withIronSessionApiRoute(withHandler("POST", handler), {
+export default withIronSessionApiRoute(withHandler("GET", handler), {
   cookieName: "carrotsession",
   password:
     "9845904809485098594385093840598df;slkgjfdl;gkfsdjg;ldfksjgdsflgjdfklgjdflgjflkgjdgd",
