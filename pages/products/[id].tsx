0bb7cdc03ca57f8c60aea688fae20a1a1e0c02ae
Inho Kang <ikang9712@gmail.com>
2023-04-14 17:35:50 -0400

    Actual data and links for UI. Basic matching word in title for related products.

--- pages/products/[id].tsx
+++ pages/products/[id].tsx
@@ -1,8 +1,25 @@
 import type { NextPage } from "next";
 import Button from "../../components/button";
 import Layout from "../../components/layout";
+import { useRouter } from 'next/router';
+import useSWR from 'swr';
+import Link from 'next/link';
+import { Product, User } from "@prisma/client";
+
+interface ProductWithUser extends Product {
+  user: User
+}
+
+interface ItemDetailResponse {
+  ok: boolean;
+  product: ProductWithUser;
+  relatedProducts: Product[]
+}
 
 const ItemDetail: NextPage = () => {
+  const router = useRouter();
+  const {data} = useSWR<ItemDetailResponse>(router.query.id ? `/api/products/${router.query.id}` : null)
+  console.log(data)
   return (
     <Layout canGoBack>
       <div className="px-4  py-4">
@@ -11,24 +28,23 @@ const ItemDetail: NextPage = () => {
           <div className="flex cursor-pointer py-3 border-t border-b items-center space-x-3">
             <div className="w-12 h-12 rounded-full bg-slate-300" />
             <div>
-              <p className="text-sm font-medium text-gray-700">Steve Jebs</p>
-              <p className="text-xs font-medium text-gray-500">
+              <p className="text-sm font-medium text-gray-700">{data?.product?.user?.name}</p>
+              <Link 
+              className="text-xs font-medium text-gray-500"
+              href={`/users/profiles/${data?.product?.user?.id}`}>
                 View profile &rarr;
-              </p>
+              </Link>
             </div>
           </div>
           <div className="mt-5">
-            <h1 className="text-3xl font-bold text-gray-900">Galaxy S50</h1>
-            <span className="text-2xl block mt-3 text-gray-900">$140</span>
+            <h1 className="text-3xl font-bold text-gray-900">
+              {data?.product?.name}
+            </h1>
+            <span className="text-2xl block mt-3 text-gray-900">
+              ${data?.product?.price}
+            </span>
             <p className=" my-6 text-gray-700">
-              My money&apos;s in that office, right? If she start giving me some
-              bullshit about it ain&apos;t there, and we got to go someplace
-              else and get it, I&apos;m gonna shoot you in the head then and
-              there. Then I&apos;m gonna shoot that bitch in the kneecaps, find
-              out where my goddamn money is. She gonna tell me too. Hey, look at
-              me when I&apos;m talking to you, motherfucker. You listen: we go
-              in there, and that ni**a Winston or anybody else is in there, you
-              the first motherfucker to get shot. You understand?
+              {data?.product?.description}
             </p>
             <div className="flex items-center justify-between space-x-2">
               <Button large text="Talk to seller" />
@@ -55,12 +71,15 @@ const ItemDetail: NextPage = () => {
         <div>
           <h2 className="text-2xl font-bold text-gray-900">Similar items</h2>
           <div className=" mt-6 grid grid-cols-2 gap-4">
-            {[1, 2, 3, 4, 5, 6].map((_, i) => (
-              <div key={i}>
-                <div className="h-56 w-full mb-4 bg-slate-300" />
-                <h3 className="text-gray-700 -mb-1">Galaxy S60</h3>
-                <span className="text-sm font-medium text-gray-900">$6</span>
-              </div>
+            {data?.relatedProducts.map((product) => (
+              <Link href={`/products/${product.id}`}>
+                <div key={product.id}>
+                  <div className="h-56 w-full mb-4 bg-slate-300" />
+                  <h3 className="text-gray-700 -mb-1">{product.name}</h3>
+                  <span className="text-sm font-medium text-gray-900">{product.price}</span>
+                </div>
+              </Link>
+              
             ))}
           </div>
         </div>
