b3e5225ea3ee75dc91f8161e5165020821965a98
Inho Kang <ikang9712@gmail.com>
2023-04-13 21:30:22 -0400

    Authentication implementation done

--- pages/enter.tsx
+++ pages/enter.tsx
@@ -1,10 +1,11 @@
 import type { NextPage } from "next";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 import Button from "../components/button";
 import Input from "../components/input";
 import useMutation from '../libs/client/useMutation';
 import { cls } from '../libs/client/utils';
+import { useRouter } from 'next/router';
 
 
 interface EnterForm {
@@ -45,6 +46,12 @@ const Enter: NextPage = () => {
     if (tokenLoading) return;
     confirmToken(tokenForm);
   };
+  const router = useRouter();
+  useEffect(() => {
+    if (tokenData?.ok){
+      router.push("/");
+    }
+  }, [tokenData, router])
   return (
     <div className="mt-16 px-4">
       <h3 className="text-3xl font-bold text-center">Enter to Carrot</h3>
