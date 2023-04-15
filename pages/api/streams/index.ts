e91961df693d9ad5bf00495daad03c6ffe144ef3
Inho Kang <ikang9712@gmail.com>
2023-04-15 16:00:00 -0400

    pagination (needs to be implemented globally)

--- pages/api/streams/index.ts
+++ pages/api/streams/index.ts
@@ -13,7 +13,10 @@ async function handler(
         body: {name,price,description},
     } = req
     if (req.method === "GET") {
-        const streams = await client.stream.findMany();
+        const streams = await client.stream.findMany({
+            take: 10,
+            // skip: 10,
+        });
         res.json({
             ok: true,
             streams
